67ddfa52712e4615f3d62d1ff8555d6a7596c823
JinXin <1152300755@qq.com>
2020-04-24 23:52:38 +0800

    环形链表

---
+++ src/链表/_141_环形链表.java
@@ -0,0 +1,37 @@
+package 链表;
+
+public class _141_环形链表 { 
+	/** 思想：快慢指针 */  
+	
+	/** 我的实现 */
+	// 分析：
+	// 1.缺少临界值判断
+	// 2.直接return即可 用break太垃圾了
+	// 3.while (fast.next != null) 缺少判断fast != null
+    public boolean hasCycle(ListNode head) { 
+    	ListNode slow = head;
+    	ListNode fast = head.next;
+    	Boolean flag = false;
+    	while (fast.next != null) {
+    		slow = slow.next;
+    		fast = fast.next.next;
+    		if (slow == fast) {
+				flag = true;
+                break;
+			}
+		}
+    	return flag;
+    }
+	 
+	/** 标准答案 */
+    public boolean hasCycle1(ListNode head) { 
+    	ListNode slow = head;
+    	ListNode fast = head.next; 
+    	while (fast != null && fast.next != null) {
+    		if (slow == fast) return true; 
+    		slow = slow.next;
+    		fast = fast.next.next;
+		}
+    	return false;
+    }
+}
